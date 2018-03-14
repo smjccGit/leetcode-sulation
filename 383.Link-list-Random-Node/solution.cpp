9a3b270441d468040f74db83ddd2bc60c136c0c7
smjccGit <smjccGit@github.com>
2018-03-15 02:06:27 +0800

    C++

---
+++ 383.Link-list-Random-Node/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        srand((unsigned)time(NULL));
+        while(head != NULL){
+            numbers.push_back(head->val);
+            head=head->next;
+        }
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        return numbers[rand()%(numbers.size())]; 
+    }
+    private:
+    vector<int> numbers;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
